df6342b4408d2d3ba287ffdf1005a75990937589
conniebean <77754785+conniebean@users.noreply.github.com>
2023-11-24 22:26:31 -0500

    make all pages uniform and add more styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,24 +3,13 @@ module.exports = {
     content: ["./*.{html,js}"],
     theme: {
         extend: {},
-    },
-    daisyui: {
-        themes: [
-            {
-                myTheme: {
-                    "primary": "#a991f7",
-                    "secondary": "#f6d860",
-                    "accent": "#37cdbe",
-                    "neutral": "#3d4451",
-                    "base-100": "#ffffff",
-                },
-            },
-            "dark",
-            "cupcake",
-            "autumn",
-            "valentine",
-        ],
-    },
-    plugins: [require("daisyui")],
+        daisyui: {
+            themes: [
+                "dim",
+                "nord",
+                "garden",
+            ],
+        },
+        plugins: [require("daisyui")],
+    }
 }
-
